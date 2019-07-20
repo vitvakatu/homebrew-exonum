eaa8345adb807aaac73e48c3b1968fe2675a42b0
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2019-07-20 16:29:45 +0300

    Update revision

--- Formula/exonum-java.rb
+++ Formula/exonum-java.rb
@@ -3,6 +3,7 @@ class ExonumJava < Formula
   homepage "https://exonum.com"
   url "https://github.com/exonum/exonum-java-binding/releases/download/ejb%2Fv0.7.0/exonum-java-0.7.0-release-Mac.zip"
   sha256 "55e8337d2d10c114bcd31e59f9bf4fffbd9522579a074bd2de29767d26f1bcfb"
+  revision 1
 
   depends_on :java
   depends_on "libsodium"
