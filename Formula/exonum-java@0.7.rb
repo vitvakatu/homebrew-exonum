2d80f206338cc645087998e8751be3ce02f236b8
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2019-09-09 18:56:31 +0300

    Release 0.8.0

--- Formula/exonum-java@0.7.rb
+++ Formula/exonum-java@0.7.rb
@@ -1,8 +1,8 @@
 class ExonumJava < Formula
   desc "Java runtime for Exonum blockchain"
   homepage "https://exonum.com"
-  url "https://github.com/exonum/exonum-java-binding/releases/download/ejb%2Fv0.8.0/exonum-java-0.8.0-release-Mac.zip"
-  sha256 "acdaf6305d9af7b0efb3f5cd5de6d9e3fe2ada7a0efb00623b9068e125284fdb"
+  url "https://github.com/exonum/exonum-java-binding/releases/download/ejb%2Fv0.7.0/exonum-java-0.7.0-release-Mac.zip"
+  sha256 "55e8337d2d10c114bcd31e59f9bf4fffbd9522579a074bd2de29767d26f1bcfb"
   revision 1
 
   depends_on :java
@@ -10,10 +10,15 @@ class ExonumJava < Formula
 
   def install
     prefix.install Dir["*"]
+# Remove until ECR-3368 is resolved
+    rm_f bin/"exonum-java"
     ohai "Exonum Java App installed successfully."
     ohai "Installation location is #{prefix}"
     ohai "Use the following command to set EXONUM_HOME: "
     ohai "  export EXONUM_HOME=#{prefix}"
+    ohai ""
+    ohai "Use the following command to set PATH: "
+    ohai "  export PATH=$PATH:$EXONUM_HOME/bin"
     ohai ""
     ohai "For basic documentation and usage instructions, see"
     ohai "#{prefix}/TUTORIAL.md"
