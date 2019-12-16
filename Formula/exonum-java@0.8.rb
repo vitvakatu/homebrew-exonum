bc3d7e07838691bdcc6727ec8a444ebd380c1c2d
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2019-12-16 10:57:25 +0300

    Release 0.9.0-rc1

--- Formula/exonum-java@0.8.rb
+++ Formula/exonum-java@0.8.rb
@@ -1,8 +1,8 @@
-class ExonumJava < Formula
+class ExonumJavaAT08 < Formula
   desc "Java runtime for Exonum blockchain"
   homepage "https://exonum.com"
-  url "https://github.com/exonum/exonum-java-binding/releases/download/ejb%2Fv0.9.0-rc1/exonum-java-0.9.0-rc1-release-Mac.zip"
-  sha256 "b607fd8e2a4c15b6e8890a71826197b5ed70528ea209d5cf29e7f5f1da74eedb"
+  url "https://github.com/exonum/exonum-java-binding/releases/download/ejb%2Fv0.8.0/exonum-java-0.8.0-release-Mac.zip"
+  sha256 "acdaf6305d9af7b0efb3f5cd5de6d9e3fe2ada7a0efb00623b9068e125284fdb"
 
   depends_on :java
   depends_on "libsodium"
@@ -15,6 +15,8 @@ class ExonumJava < Formula
     ohai "  export EXONUM_HOME=#{prefix}"
     ohai ""
     ohai "For basic documentation and usage instructions, see"
+    ohai "#{prefix}/TUTORIAL.md"
+    ohai "or"
     ohai "https://exonum.com/doc/version/latest/get-started/java-binding"
   end
 end
